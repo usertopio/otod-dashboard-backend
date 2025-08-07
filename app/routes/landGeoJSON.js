4ee870a9bad24a877f5c4051e4933a92156b30af
usertop <tammasorn.tammasorn@mail.kmutt.ac.th>
2025-08-07 16:41:59 +0700

    refactor(db): 🔥remove deprecated database configuration and service files
    📝 What Changed
    - Deleted obsolete configuration files for landGeoJSON and lands.
    - Removed associated service and controller files.
    ❓ Why It Changed
    - These files were no longer in use and contributed to code clutter.
    ⚙️ How It Works
    - The removal streamlines the codebase, improving maintainability and readability.
    ⚠️ Side Effects or Risks
    - Ensure that no existing functionality relies on these deleted files.
    🔗 Additional Context
    - This change is part of an effort to clean up the codebase and improve overall project structure.

--- app/routes/landGeoJSON.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { fetchLandGeoJSON } = require("../controllers/landGeoJSON");
-
-router.post("/fetchLandGeoJSON", fetchLandGeoJSON);
-
-module.exports = router;
--- app/routes/lands.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { fetchLands } = require("../controllers/lands");
-
-// Fetch Lands data from the outsource API
-router.post("/fetchLands", fetchLands);
-
-module.exports = router;
