33f5c4fc49dd27f2b69add0b3045be277fbc468f
usertopio <tammasorn.dev@gmail.com>
2026-02-06 10:03:34 +0700

    feat(cron):  ✨run every 5 AM

--- app/utils/cronUtils.js
+++ app/utils/cronUtils.js
@@ -1,5 +1,5 @@
 export const SCHEDULES_CONFIG = {
-  // SCHEDULE_1: "0 0 5 * * *", // every 5 AM
+  SCHEDULE_1: "0 0 5 * * *", // every 5 AM
   // SCHEDULE_2: "*/60 * * * * *", // every 1 min
-  SCHEDULE_3: "*/3 * * * *", // every 3 min
+  // SCHEDULE_3: "*/3 * * * *", // every 3 min
 };
